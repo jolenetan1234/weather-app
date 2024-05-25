cae8a11449b2e478adcb0b2887b1961e32f830cc
jolene <jolneetanky@gmail.com>
2024-05-25 13:08:52 +0800

    Set up 'submit' event and troubleshooted

--- index.js
+++ index.js
@@ -2,19 +2,52 @@
 
 // handle form submission
 const weatherForm = document.querySelector("#weatherForm");
+weatherForm.addEventListener("submit", (e) => handleSubmitPromise(e));
 
-const handleSubmit = (form) => {
-
+const SubmitPromise = (event) => {
     // here we define what data we want etc.
-    city = form.querySelector('input').value;
+    event.preventDefault();
 
-    // callback function
-    fetchData(city);
+    const form = event.target;
+    const city = form.querySelector('input').value; // event.target refers to the <form> element
+    // city = form.querySelector('input').value;
+    let p = new Promise((resolve, reject) => {
+        if (city != "") {
+            resolve(city);
+        } else {
+            reject("Input cannot be empty.");
+        };
+    });
+    return p;
+};
+
+const handleSubmitPromise = (event) => {
+    // handle promise
+    SubmitPromise(event).then((res) => {
+        console.log(`Success: ${res}`);
+    }).catch((msg) => {
+        alert(msg);
+    });
 }
 
-weatherForm.addEventListener("submit", () => handleSubmit(weatherForm));
+// handle promise
+// SubmitPromise(event).then( (res) => {
+//     console.log(`Success: ${res}`);
+// }).catch((msg) => {
+//     alert(msg);
+// })
+    
+
+
+
+    // callback function
+    // fetchData(city);
 
 // fetching the data
+// const fetchData = (city) => {
+//     let url = 
+// }
+
 fetch("https://api.weatherapi.com/v1/current.json?key=5146eba384ca42b1a2792606242305&q=london", { mode: "cors" }) 
 // `fetch` returns a promise that resolves to a Response object. => ie. in the code for fetch, we see `resolve` being called with `resolve(res)`
 // In the Response object, res.json() is ANOTHER promise that resolves to the parsed JSON data.
